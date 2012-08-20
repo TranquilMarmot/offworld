3decf8531b6ac5a892acd350c5080cd09019bbe0
Nate Moore <nate272@gmail.com>
2012-08-20 11:01:24 -0600

    Made it so that the player's arms rotate correctly and no longer look totally ridiculous

--- src/com/bitwaffle/moguts/graphics/render/renderers/PlayerRenderer.java
+++ src/com/bitwaffle/moguts/graphics/render/renderers/PlayerRenderer.java
@@ -2,11 +2,9 @@ package com.bitwaffle.moguts.graphics.render.renderers;
 
 import android.opengl.Matrix;
 
-import com.badlogic.gdx.math.Vector2;
 import com.bitwaffle.moguts.entities.Entity;
 import com.bitwaffle.moguts.graphics.render.Render2D;
 import com.bitwaffle.moguts.util.BufferUtils;
-import com.bitwaffle.moguts.util.MathHelper;
 import com.bitwaffle.offworld.Game;
 import com.bitwaffle.offworld.entities.Player;
 
@@ -29,12 +27,12 @@ class PlayerRenderer implements EntityRenderer{
 	
 	/** Location of arms */
 	private final float
-		L_ARM_X_OFFSET = 0.37f,
-		L_ARM_Y_OFFSET = 0.35f,
-		R_ARM_X_OFFSET = 0.5f,
-		R_ARM_Y_OFFSET = 0.3f,
-		ARM_ROTATION_X_OFFSET = 0.2f,
-		ARM_ROTATION_Y_OFFSET = 0.2f;
+		L_ARM_X_OFFSET = 0.1f,
+		L_ARM_Y_OFFSET = 0.65f,
+		R_ARM_X_OFFSET = 0.15f,
+		R_ARM_Y_OFFSET = 0.6f,
+		ARM_ROTATION_X_OFFSET = 0.33f,
+		ARM_ROTATION_Y_OFFSET = -0.3f;
 	
 	/** Scale everything is being drawn at (to fit into bounding box) */
 	private final float SCALE = 0.95f;
@@ -54,8 +52,16 @@ class PlayerRenderer implements EntityRenderer{
 		boolean movingRight = player.isMovingRight();
 		boolean facingRight = player.isFacingRight();
 		
-		/*-- Render Legs --*/
+		/*-- Render right arm --*/
 		BufferUtils.deepCopyFloatArray(renderer.modelview, oldMatrix);
+		Matrix.translateM(renderer.modelview, 0, facingRight ? R_ARM_X_OFFSET : -R_ARM_X_OFFSET, R_ARM_Y_OFFSET, 0.0f);
+		Matrix.rotateM(renderer.modelview, 0, player.getArmAngle(), 0.0f, 0.0f, 1.0f);
+		Matrix.translateM(renderer.modelview, 0, ARM_ROTATION_X_OFFSET, facingRight ? ARM_ROTATION_Y_OFFSET : -ARM_ROTATION_Y_OFFSET, 0.0f);
+		renderer.sendModelViewToShader();
+		Game.resources.textures.getSubImage("playerarm").draw(renderer.quad, 0.5f * SCALE, 0.317f * SCALE, !facingRight, facingRight);
+		
+		/*-- Render Legs --*/
+		BufferUtils.deepCopyFloatArray(oldMatrix, renderer.modelview);
 		// if movingRight and facingRight are the same, it means the player is moving in the direction it's looking
 		if(movingRight == facingRight)
 			Matrix.translateM(renderer.modelview, 0, movingRight ? FORWARD_LEGS_X_OFFSET : -FORWARD_LEGS_X_OFFSET, LEGS_Y_OFFSET, 0.0f);
@@ -65,14 +71,6 @@ class PlayerRenderer implements EntityRenderer{
 		renderer.sendModelViewToShader();
 		player.animation.renderCurrentFrame(renderer, 1.0f * SCALE, 0.902f * SCALE, movingRight, false);
 		
-		/*-- Render right arm --*/
-		BufferUtils.deepCopyFloatArray(oldMatrix, renderer.modelview);
-		Matrix.translateM(renderer.modelview, 0, facingRight ? R_ARM_X_OFFSET : -R_ARM_X_OFFSET, R_ARM_Y_OFFSET, 0.0f);
-		Matrix.rotateM(renderer.modelview, 0, player.getArmAngle(), 0.0f, 0.0f, 1.0f);
-		//Matrix.translateM(renderer.modelview, 0, facingRight ? ARM_ROTATION_X_OFFSET : ARM_ROTATION_X_OFFSET, facingRight ? ARM_ROTATION_Y_OFFSET : -ARM_ROTATION_Y_OFFSET, 0.0f);
-		renderer.sendModelViewToShader();
-		Game.resources.textures.getSubImage("playerarm").draw(renderer.quad, 0.5f * SCALE, 0.317f * SCALE, !facingRight, facingRight);
-		
 		/*-- Render body --*/
 		BufferUtils.deepCopyFloatArray(oldMatrix, renderer.modelview);
 		Matrix.translateM(renderer.modelview, 0, facingRight ? BODY_X_OFFSET : -BODY_X_OFFSET, BODY_Y_OFFSET, 0.0f);
@@ -81,10 +79,9 @@ class PlayerRenderer implements EntityRenderer{
 		
 		/*-- Render left arm --*/
 		BufferUtils.deepCopyFloatArray(oldMatrix, renderer.modelview);
-		//Matrix.translateM(renderer.modelview, 0, facingRight ? ARM_ROTATION_X_OFFSET : ARM_ROTATION_X_OFFSET, ARM_ROTATION_Y_OFFSET, 0.0f);
 		Matrix.translateM(renderer.modelview, 0, facingRight ? L_ARM_X_OFFSET : -L_ARM_X_OFFSET, L_ARM_Y_OFFSET, 0.0f);
 		Matrix.rotateM(renderer.modelview, 0, player.getArmAngle(), 0.0f, 0.0f, 1.0f);
-		//Matrix.translateM(renderer.modelview, 0, facingRight ? ARM_ROTATION_X_OFFSET : ARM_ROTATION_X_OFFSET, facingRight ? ARM_ROTATION_Y_OFFSET : -ARM_ROTATION_Y_OFFSET, 0.0f);
+		Matrix.translateM(renderer.modelview, 0, ARM_ROTATION_X_OFFSET, facingRight ? ARM_ROTATION_Y_OFFSET : -ARM_ROTATION_Y_OFFSET, 0.0f);
 		renderer.sendModelViewToShader();
 		Game.resources.textures.getSubImage("playerarm").draw(renderer.quad, 0.5f * SCALE, 0.317f * SCALE, !facingRight, facingRight);
 	}
