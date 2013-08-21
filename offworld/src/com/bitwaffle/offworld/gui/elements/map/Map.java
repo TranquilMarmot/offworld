0259b1945e4945f5690f0f3b10225604d61d118b
TranquilMarmot <nate272@gmail.com>
2013-08-21 18:14:21 -0400

    Fixed map to render only in the correct viewport with the corrent aspect ratio (temporary, only works on top half splitscreen)

--- offworld/src/com/bitwaffle/offworld/gui/elements/map/Map.java
+++ offworld/src/com/bitwaffle/offworld/gui/elements/map/Map.java
@@ -2,11 +2,14 @@ package com.bitwaffle.offworld.gui.elements.map;
 
 import java.util.Iterator;
 
+import com.badlogic.gdx.Gdx;
 import com.bitwaffle.guts.Game;
 import com.bitwaffle.guts.entity.Entity;
 import com.bitwaffle.guts.graphics.Renderer;
 import com.bitwaffle.guts.gui.elements.GUIObject;
 import com.bitwaffle.guts.physics.Room;
+import com.bitwaffle.guts.util.MathHelper;
+import com.bitwaffle.offworld.OffworldGame;
 
 public class Map extends GUIObject {
 
@@ -23,15 +26,25 @@ public class Map extends GUIObject {
 		Room room = Game.physics.currentRoom();
 		Iterator<Entity> geomIt = room.getStaticGeometryIterator();
 		
-		renderer.modelview.scale(0.5f, 0.5f, 1.0f);
+		Game.windowHeight /= 2;
+		Game.aspect = (float) Game.windowWidth / (float) Game.windowHeight;
+		Gdx.gl.glViewport(0, Game.windowHeight, Game.windowWidth, Game.windowHeight);
+		MathHelper.orthoM(renderer.projection, 0, Game.windowWidth, Game.windowHeight, 0, -1, 1);
+		
+		renderer.modelview.scale(10.0f, 10.0f, 1.0f);
 		renderer.modelview.rotate(0.0f, 0.0f, 1.0f, 180.0f);
 		renderer.modelview.rotate(0.0f, 1.0f, 0.0f, 180.0f);
+		renderer.modelview.translate(-OffworldGame.players[0].getLocation().x, -OffworldGame.players[0].getLocation().y, 0.0f);
 		renderer.r2D.sendMatrixToShader();
 		while(geomIt.hasNext()){
 			Entity ent = geomIt.next();
 			ent.renderer.render(renderer, ent, true);
 		}
 		
+		Game.windowHeight *= 2;
+		Game.aspect = (float) Game.windowWidth / (float) Game.windowHeight;
+		Gdx.gl.glViewport(0, 0, Game.windowWidth, Game.windowHeight);
+		MathHelper.orthoM(renderer.projection, 0, Game.windowWidth, Game.windowHeight, 0, -1, 1);
 	}
 
 	@Override
